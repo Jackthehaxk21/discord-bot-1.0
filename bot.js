337dbe7dd572e778e4ca9eeae6ec02ba6cd75067
Jackthehaxk21 <32798243+Jackthehaxk21@users.noreply.github.com>
2017-10-15 09:15:31 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,6 +8,7 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
+        message.channel.send('Pong!');
         console.log('pinged !')
   	}
 });
