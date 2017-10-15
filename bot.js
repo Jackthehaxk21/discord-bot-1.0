8c50189484e97f55232bf0352e91dd19281397e0
Jackthehaxk21 <32798243+Jackthehaxk21@users.noreply.github.com>
2017-10-15 11:35:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 var startup = 0;
 client.on('ready', () => {
     console.log('I am ready!');
-    message.channel.send("BOOTED")
+    //message.channel.send("BOOTED") DOES NOT WORK UNDEFINED
     startup = 1;
 });
 
