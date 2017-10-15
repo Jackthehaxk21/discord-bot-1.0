dff8f9115bcabdf7dd0a164ed3c3c1fc0cab1d06
Jackthehaxk21 <32798243+Jackthehaxk21@users.noreply.github.com>
2017-10-15 09:26:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,13 +6,19 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
+    if (message.content === '!ping') {
     	message.reply('pong');
         message.channel.send('Pong!');
         console.log('pinged !')
   	}
-    if (message.content === 'help') {
-        message.author.send("Your message here.")
+    if (message.content === '!help') {
+        message.author.send("Help Documentation for JACKTHEHACK21 (BOT)")
+        message.author.send("------------------------------------------")
+        message.author.send("")
+        message.author.send("!ping - see how fast it takes me to pong !")
+    }
+    else {
+        message.reply('Uknown Command !')
     }
 });
 
