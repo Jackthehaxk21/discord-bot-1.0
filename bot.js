a19716419471c6ca5b2797a8643fe2d7cc7f3ab1
Jackthehaxk21 <32798243+Jackthehaxk21@users.noreply.github.com>
2017-10-15 10:58:20 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+const startup = '0';
 client.on('ready', () => {
     console.log('I am ready!');
+    startup = '1';
 });
 
 client.on('message', message => {
+    if (startup === '1') {
+        message.channel.send("Booted and ready to use !")
+        startup = '0';
+    }
     if (message.content === '!ping') {
     	message.reply('pong');
         message.channel.send('Pong!');
@@ -16,7 +21,6 @@ client.on('message', message => {
         message.reply('Documentation has been sent to you via Private Message (PM)')
         message.author.send("Help Documentation for JACKTHEHACK21 (BOT)")
         message.author.send("------------------------------------------")
-        message.author.send("{NL}")
         message.author.send("!ping - see how fast it takes me to pong !")
     }
 });
