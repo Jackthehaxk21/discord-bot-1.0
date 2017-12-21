cf3f0c27c22877cb4586e9b1ebb697b960bb02e1
Jackthehaxk21 <32798243+Jackthehaxk21@users.noreply.github.com>
2017-12-21 17:58:31 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -77,6 +77,22 @@ client.on('message', message => {
         case "removerole" :
             message.channel.send("test");
             break;
+        case "purge" :
+            const user = message.mentions.users.first();
+            const amount = !!parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2])
+            if (!amount) return message.reply('Must specify an amount to delete!');
+            if (!amount && !user) return message.reply('Must specify a user and amount, or just an amount, of messages to purge!');
+            message.channel.fetchMessages({
+                limit: amount,
+            }).then((messages) => {
+                if (user) {
+                    const filterBy = user ? user.id : Client.user.id;
+                    messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
+                }
+                message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
+            });
+            break;
+        
     }
     /*message.reply(message.content);
     if (!points[message.author.id]) points[message.author.id] = {
