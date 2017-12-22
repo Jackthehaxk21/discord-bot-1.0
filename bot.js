2a890545f1131d614ef16efd6fbe6bd003490d9d
Jackthehaxk21 <32798243+Jackthehaxk21@users.noreply.github.com>
2017-12-22 19:03:57 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 var startup = 0;
 const fs = require("fs");
 
-let points = JSON.parse(fs.readFileSync("./points.json", "utf8"));
+let points = JSON.parse(fs.readFileSync("https://www.jasonbase.com/things/DnBo/edit#", "utf8"));
 const prefix = "!";
 
 client.on("guildMemberAdd", (member) => {
