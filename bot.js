3c22ecb681cccc682e1df9e6f3506516427cc67b
Jackthehaxk21 <32798243+Jackthehaxk21@users.noreply.github.com>
2017-12-22 14:43:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -144,11 +144,11 @@ client.on('message', message => {
                 break;
             // Let's first check if we have a member and if we can kick them!
             // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
-            let user = message.mentions.members.first();
-            if(!user)
+            let toKick = message.mentions.members.first();
+            if(!toKick)
                 message.reply("Please mention a valid member of this server");
                 break;
-            if(!user.kickable) 
+            if(!toKick.kickable) 
                 message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
                 break;
             // slice(1) removes the first part, which here should be the user mention!
@@ -158,11 +158,11 @@ client.on('message', message => {
                 break;
             // Now, time for a swift kick in the nuts!
             try {
-              user.kick(reason)
+                toKick.kick(reason)
             } catch (e) {
                 message.reply(`Sorry ${message.author} I couldn't kick because of : ${e}`);
             }
-            message.reply(`${user.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+            message.reply(`${toKick.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
             break;
         case "purge" :
             const user = message.mentions.users.first();
