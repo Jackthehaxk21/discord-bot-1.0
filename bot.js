00fdb862d92a906982afe0e025ec5a18ab7caf10
Jackthehaxk21 <32798243+Jackthehaxk21@users.noreply.github.com>
2017-12-23 19:26:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,18 @@ const client = new Discord.Client();
 var startup = 0;
 const fs = require("fs");
 
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
 //let points = JSON.parse(fs.readFileSync("https://www.jasonbase.com/things/DnBo.json ", "utf8"));
 const prefix = "!";
 
@@ -29,6 +41,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
+  
     if (!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift();
