f179ffa1d2f5319ee06796567e1ba9e49157ef4f
Glitch (discord-mk-bot) <none>
2018-01-02 10:16:08 +0000

    🚉🎯 Small fix

--- Data/Commands/level.js
+++ Data/Commands/level.js
@@ -3,16 +3,16 @@ var methods = {
       sql.get(`SELECT * FROM scores WHERE userId = "${message.guild.id+message.author.id}"`).then(row => {
         if (!row) {
           sql.run("INSERT INTO scores (userId, points, level) VALUES (?, ?, ?)", [message.guild.id + message.author.id, 1, 0]);
-          message.channel.send("**Level:**\n**Level: **0\n**XP: **1");
+          message.channel.send("**"+message.author.username+":**\n**Level: **0\n**XP: **1");
         } else {
-          message.channel.send("**Level:**\n**Level: **"+row.level+"\n**XP: **"+row.points);
+          message.channel.send("**"+message.author.username+":**\n**Level: **"+row.level+"\n**XP: **"+row.points);
         }
       }).catch(() => {
         console.error;
         //console.log('new t');
         sql.run("CREATE TABLE IF NOT EXISTS scores (userId TEXT, points INTEGER, level INTEGER)").then(() => {
           sql.run("INSERT INTO scores (userId, points, level) VALUES (?, ?, ?)", [message.guild.id + message.author.id, 1, 0]);
-          message.channel.send("**Level:**\n**Level: **0\n**XP:** 1");
+          message.channel.send("**"+message.author.username+":**\n**Level: **0\n**XP:** 1");
         });
       });
   }
